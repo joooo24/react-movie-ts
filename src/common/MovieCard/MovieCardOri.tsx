801f9ed6edf410e898c027959803d38e489f5b99
주현정 <61533589+joooo24@users.noreply.github.com>
2024-04-30 18:01:56 +0900

    add: movie 카드/슬라이드 오리지널 추가

---
+++ src/common/MovieCard/MovieCardOri.tsx
@@ -0,0 +1,74 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Badge } from "react-bootstrap";
+import "./MovieCard.scss";
+import { useMoviesGenreQuery, Genre } from "../../hooks/useMovieGenre";
+
+interface Movie {
+    id: number;
+    title: string;
+    poster_path: string | null;
+    genre_ids: number[];
+    vote_average: number;
+    popularity: number;
+    adult: boolean;
+    release_date: string;
+}
+
+interface Props {
+    movie: Movie;
+}
+
+const MovieCard: React.FC<Props> = ({ movie }) => {
+    // 네비게이트
+    const navigate = useNavigate();
+
+    // 뮤비카드 클릭했을 때 id값으로 URL 이동
+    const handleMovieCard = () => {
+        const url = `/movies/${movie.id}`;
+        // URL로 이동
+        navigate(url);
+    };
+
+    // 카드 이미지 URL 정의
+    const backgroundImageUrl = movie?.poster_path
+        ? `https://media.themoviedb.org/t/p/w600_and_h900_bestv2${movie.poster_path}`
+        : "";
+
+    // 장르 정의
+    const { data: genreData } = useMoviesGenreQuery();
+    const showGenre = (genreIdList: number[]): string[] => {
+        if (!genreData) return [];
+        const genreNameList = genreIdList?.map((id) => {
+            const genreObj = genreData.find((genre: Genre) => genre.id === id);
+            return genreObj.name;
+        });
+
+        return genreNameList;
+    };
+
+    return (
+        <div
+            onClick={handleMovieCard}
+            className="movie-card"
+            style={{
+                backgroundImage: `url(${backgroundImageUrl})`,
+            }}
+        >
+            <div className="overlay">
+                <h1>{movie.title}</h1>
+                {showGenre(movie.genre_ids).map((genre, index) => (
+                    <Badge bg="danger" key={index}>
+                        {genre}
+                    </Badge>
+                ))}
+                <div>{movie.vote_average}점</div>
+                <div>인기: {movie.popularity}</div>
+                <div>{movie.adult ? "over18" : ""}</div>
+                <div>{movie.release_date}</div>
+            </div>
+        </div>
+    );
+};
+
+export default MovieCard;
---
+++ src/common/MovieSlider/MovieSliderOri.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import MovieCard from "../MovieCard/MovieCard";
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
+import "./MovieSlider.scss";
+// import { responsive } from "../constants/responsive";
+
+interface MovieSliderProps {
+    data: { results: any[] };
+    title: string;
+}
+
+// ResponsiveType 정의
+interface ResponsiveType {
+    [key: string]: {
+        breakpoint: { max: number; min: number };
+        items: number;
+    };
+}
+
+const MovieSlider: React.FC<MovieSliderProps> = ({ data, title }) => {
+
+    const responsive: ResponsiveType = {
+        desktop: {
+            breakpoint: { max: 3000, min: 1024 },
+            items: 6,
+        },
+        tablet: {
+            breakpoint: { max: 1024, min: 464 },
+            items: 2,
+        },
+        mobile: {
+            breakpoint: { max: 464, min: 0 },
+            items: 1,
+        },
+    };
+
+    return (
+        <div className="movie-slider-container">
+            <h3 className="">{title}</h3>
+            <Carousel
+                infinite={true}
+                centerMode={true}
+                itemClass="movie-slider p-1"
+                containerClass="carousel-container"
+                responsive={responsive}
+            >
+                {data?.results.map((movie: any, index: number) => (
+                    <MovieCard key={movie.id} movie={movie}></MovieCard>
+                ))}
+            </Carousel>
+        </div>
+    );
+};
+
+export default MovieSlider;
