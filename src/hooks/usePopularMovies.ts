76a115fbce2b224e3b478797145c284fcd24ad8b
주현정 <61533589+joooo24@users.noreply.github.com>
2024-04-30 15:22:30 +0900

    add: 훅 생성 usePopularMovies.ts

---
+++ src/hooks/usePopularMovies.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+import api from "../utils/api";
+
+type MovieData = any;
+
+const fetchPopularMovies = async (): Promise<MovieData> => {
+    const response = await api.get<MovieData>("/movie/popular");
+    return response.data;
+};
+
+export const usePopularMoviesQuery = () => {
+    return useQuery<MovieData, Error>({
+        queryKey: ["movie-popular"],
+        queryFn: fetchPopularMovies,
+        select: (result) => result.data,
+    });
+};
