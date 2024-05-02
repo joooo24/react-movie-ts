0c5bdf83dafdd7c2b5ca3d8d1e8c3bae6fab97fa
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-02 15:02:16 +0900

    chore: 카드슬라이더,카드 분리 1차

--- src/common/MovieCard/MovieCard.tsx
+++ src/common/MovieCard/MovieCard.tsx
@@ -1,36 +1,52 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import { Badge } from "react-bootstrap";
 import "./MovieCard.scss";
-import { useMoviesGenreQuery, Genre } from "../../hooks/useMovieGenre";
 
 interface Movie {
-    id: number;
-    title: string;
     poster_path: string | null;
-    genre_ids: number[];
+    title: string;
+    genre_ids: any[];
     vote_average: number;
     popularity: number;
     adult: boolean;
-    release_date: string;
 }
 
-interface Props {
+interface MovieCardProps {
     movie: Movie;
 }
 
-const MovieCard: React.FC<Props> = ({ movie }) => {
+const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
+
+    const getBackgroundImageUrl = (posterPath: string | null) => {
+        return posterPath ? `https://media.themoviedb.org/t/p/w600_and_h900_bestv2${posterPath}` : "";
+    };
+
     return (
-        <div>
+        <div
+            className="movie-card"
+            style={{
+                backgroundImage: `url(${getBackgroundImageUrl(movie.poster_path)})`,
+            }}
+        >
             <div className="overlay">
-                <h1>뮤비타이틀</h1>
-                    <Badge bg="danger">
-                        장르
-                    </Badge>
-                <div>평점</div>
-                <div>인기</div>
-                <div>연령</div>
-                <div>개봉일</div>
+                <h1>{movie.title}</h1>
+                <ul className="badge-wrap">
+                    {movie.genre_ids.map((genre: any, idx: number) => (
+                        <li key={idx}>{genre}</li>
+                    ))}
+                </ul>
+                <div className="title">{movie.title}</div>
+                <ul className="tag-wrap">
+                    <li className="">
+                        <i></i>
+                        {movie.vote_average}점
+                    </li>
+                    <li className="">
+                        <i></i>
+                        {movie?.popularity}
+                    </li>
+                    <li>{movie.adult ? <div className="adult">18</div> : <div className="all">ALL</div>}</li>
+                </ul>
             </div>
         </div>
     );
--- src/common/MovieSlider/MovieSlider.tsx
+++ src/common/MovieSlider/MovieSlider.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "react-multi-carousel/lib/styles.css";
 import "./MovieSlider.scss";
+import MovieCard from "../MovieCard/MovieCard";
 // import { responsive } from "../constants/responsive";
 
 interface MovieSliderProps {
@@ -11,42 +12,12 @@ interface MovieSliderProps {
 const MovieSlider: React.FC<MovieSliderProps> = ({ data, title }) => {
     console.log("### MovieSlider usePopularMoviesQuery data", data);
 
-    const getBackgroundImageUrl = (posterPath: string | null) => {
-        return posterPath ? `https://media.themoviedb.org/t/p/w600_and_h900_bestv2${posterPath}` : "";
-    };
 
     return (
         <div className="movie-slider-container">
             <h3 className="">{title}</h3>
             {data?.results.map((movie: any, idx: number) => (
-                <div
-                    key={idx}
-                    className="movie-card"
-                    style={{
-                        backgroundImage: `url(${getBackgroundImageUrl(movie.poster_path)})`,
-                    }}
-                >
-                    <div className="overlay">
-                        <h1>{movie.title}</h1>
-                        <ul className="badge-wrap">
-                            {movie.genre_ids.map((genre: any, idx: number) => (
-                                <li key={idx}>{genre}</li>
-                            ))}
-                        </ul>
-                        <div className="title">{movie.title}</div>
-                        <ul className="tag-wrap">
-                            <li className="">
-                                <i></i>
-                                {movie.vote_average}점
-                            </li>
-                            <li className="">
-                                <i></i>
-                                {movie?.popularity}
-                            </li>
-                            <li>{movie.adult ? <div className="adult">18</div> : <div className="all">ALL</div>}</li>
-                        </ul>
-                    </div>
-                </div>
+                <MovieCard movie={movie}  key={idx}/>
             ))}
         </div>
     );
