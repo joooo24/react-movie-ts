2a320f123094f8143f6e5d887ca755ff7f980b88
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-02 15:28:38 +0900

    chore: movie 슬라이더/카드 인터페이스 수정

--- src/common/MovieCard/MovieCard.tsx
+++ src/common/MovieCard/MovieCard.tsx
@@ -1,11 +1,16 @@
+// MovieCard.tsx
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import "./MovieCard.scss";
+
+// interface Genre {
+//     id: number;
+//     name: string;
+// }
 
 interface Movie {
     poster_path: string | null;
     title: string;
-    genre_ids: any[];
+    genre_ids: number[];
     vote_average: number;
     popularity: number;
     adult: boolean;
@@ -16,10 +21,20 @@ interface MovieCardProps {
 }
 
 const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
-
     const getBackgroundImageUrl = (posterPath: string | null) => {
         return posterPath ? `https://media.themoviedb.org/t/p/w600_and_h900_bestv2${posterPath}` : "";
     };
+
+    // 장르 맵핑
+    // const genres: Genre[] = [
+    //     { id: 28, name: "액션" },
+    //     { id: 12, name: "모험" },
+    // ];
+
+    // const getGenreName = (genreId: number): string => {
+    //     const foundGenre = genres.find((genre) => genre.id === genreId);
+    //     return foundGenre ? foundGenre.name : "";
+    // };
 
     return (
         <div
@@ -31,19 +46,20 @@ const MovieCard: React.FC<MovieCardProps> = ({ movie }) => {
             <div className="overlay">
                 <h1>{movie.title}</h1>
                 <ul className="badge-wrap">
-                    {movie.genre_ids.map((genre: any, idx: number) => (
-                        <li key={idx}>{genre}</li>
+                    {movie.genre_ids.map((genreId: number, idx: number) => (
+                        // <li key={idx}>{getGenreName(genreId)}</li>
+                        <li key={idx}>{genreId}</li>
                     ))}
                 </ul>
                 <div className="title">{movie.title}</div>
                 <ul className="tag-wrap">
-                    <li className="">
+                    <li>
                         <i></i>
                         {movie.vote_average}점
                     </li>
-                    <li className="">
+                    <li>
                         <i></i>
-                        {movie?.popularity}
+                        {movie.popularity}
                     </li>
                     <li>{movie.adult ? <div className="adult">18</div> : <div className="all">ALL</div>}</li>
                 </ul>
--- src/common/MovieSlider/MovieSlider.tsx
+++ src/common/MovieSlider/MovieSlider.tsx
@@ -1,23 +1,30 @@
+// MovieSlider.tsx
 import React from "react";
-import "react-multi-carousel/lib/styles.css";
 import "./MovieSlider.scss";
 import MovieCard from "../MovieCard/MovieCard";
-// import { responsive } from "../constants/responsive";
+
+interface Movie {
+    id: number;
+    poster_path: string | null;
+    title: string;
+    genre_ids: number[];
+    vote_average: number;
+    popularity: number;
+    adult: boolean;
+    [key: string]: any; // 나머지 필드에 대한 임의의 속성
+}
 
 interface MovieSliderProps {
-    data: { results: any[] };
+    data: { results: Movie[] };
     title: string;
 }
 
 const MovieSlider: React.FC<MovieSliderProps> = ({ data, title }) => {
-    console.log("### MovieSlider usePopularMoviesQuery data", data);
-
-
     return (
         <div className="movie-slider-container">
-            <h3 className="">{title}</h3>
-            {data?.results.map((movie: any, idx: number) => (
-                <MovieCard movie={movie}  key={idx}/>
+            <h3>{title}</h3>
+            {data.results.map((movie: Movie) => (
+                <MovieCard movie={movie} key={movie.id} />
             ))}
         </div>
     );
