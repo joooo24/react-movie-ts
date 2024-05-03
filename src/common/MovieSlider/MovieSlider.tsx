ca90f77e67db605c564cc1a436ce5e04d4704899
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-03 10:54:09 +0900

    feat: ResponsiveType 공통으로 분리하여 사용

--- src/common/MovieSlider/MovieSlider.tsx
+++ src/common/MovieSlider/MovieSlider.tsx
@@ -4,6 +4,7 @@ import "./MovieSlider.scss";
 import MovieCard from "../MovieCard/MovieCard";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
+import { responsive } from "./../../common/constants/responsive";
 
 interface Movie {
     id: number;
@@ -22,31 +23,7 @@ interface MovieSliderProps {
     title: string;
 }
 
-// ResponsiveType 정의
-interface ResponsiveType {
-    [key: string]: {
-        breakpoint: { max: number; min: number };
-        items: number;
-    };
-}
-
 const MovieSlider: React.FC<MovieSliderProps> = ({ data, title }) => {
-
-    const responsive: ResponsiveType = {
-        desktop: {
-            breakpoint: { max: 3000, min: 1024 },
-            items: 6,
-        },
-        tablet: {
-            breakpoint: { max: 1024, min: 464 },
-            items: 2,
-        },
-        mobile: {
-            breakpoint: { max: 464, min: 0 },
-            items: 1,
-        },
-    };
-
     return (
         <div className="movie-slider-container">
             <h3>{title}</h3>
