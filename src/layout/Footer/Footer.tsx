70d48ae7f62fc4212ebd96c8cdbaa35aab8b8a4f
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-10 17:57:43 +0900

    chore: footer 작업중

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -1,10 +1,71 @@
-import React from 'react'
-import "./Footer.scss"
+import React from 'react';
+import "./Footer.scss";
 
 const Footer: React.FC = () => {
     return (
-        <footer>Footer</footer>
-    )
-}
+        <footer>
+            <div className="container">
+                {/* <h2 className="blind">푸터</h2> */}
+                <div className="footer-inner">
+                    <div className="footer-menu">
+                        <div>
+                            <h3>향수 사이트</h3>
+                            <ul>
+                                <li><a href="#">소개하기</a></li>
+                                <li><a href="#">향수 이미지</a></li>
+                                <li><a href="#">향수 지속시간</a></li>
+                                <li><a href="#">나만의 향수</a></li>
+                                <li><a href="#">커스텀 방법</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>향수 사이트</h3>
+                            <ul>
+                                <li><a href="#">소개하기</a></li>
+                                <li><a href="#">향수 이미지</a></li>
+                                <li><a href="#">향수 지속시간</a></li>
+                                <li><a href="#">나만의 향수</a></li>
+                                <li><a href="#">커스텀 방법</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>향수 사이트</h3>
+                            <ul>
+                                <li><a href="#">소개하기</a></li>
+                                <li><a href="#">향수 이미지</a></li>
+                                <li><a href="#">향수 지속시간</a></li>
+                                <li><a href="#">나만의 향수</a></li>
+                                <li><a href="#">커스텀 방법</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>ranguage</h3>
+                            <ul>
+                                <li><a href="#">React.js</a></li>
+                                <li><a href="#">Typescript</a></li>
+                                <li><a href="#">Node.js</a></li>
+                                <li><a href="#">MongoDB</a></li>
+                                <li><a href="#">Express</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <h3>향수 사이트</h3>
+                            <ul>
+                                <li><a href="#">소개하기</a></li>
+                                <li><a href="#">향수 이미지</a></li>
+                                <li><a href="#">향수 지속시간</a></li>
+                                <li><a href="#">나만의 향수</a></li>
+                                <li><a href="#">커스텀 방법</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <address className="footer-right">
+                        2024 주현정 Portfolio. React Typescript.<br />All Right Reserved
+                    </address>
+                </div>
+            </div>
+        </footer>
+    );
+};
 
-export default Footer
+export default Footer;
