8ed5f52d41069c882e7b20ef383963d781f0feb1
주현정 <61533589+joooo24@users.noreply.github.com>
2024-05-13 13:28:20 +0900

    chore: 푸터 내용 수정

--- src/layout/Footer/Footer.tsx
+++ src/layout/Footer/Footer.tsx
@@ -4,59 +4,43 @@ import "./Footer.scss";
 const Footer: React.FC = () => {
     return (
         <footer>
-                {/* <h2 className="blind">푸터</h2> */}
-                <div className="footer-inner">
-                    <div className="footer-menu">
-                        <div>
-                            <h3>Frontend</h3>
-                            <ul>
-                                <li><a href="#">Vue.js</a></li>
-                                <li><a href="#">Nuxt.js</a></li>
-                                <li><a href="#">React.js</a></li>
-                                <li><a href="#">Next.js</a></li>
-                                <li><a href="#">TypeScript</a></li>
-                                <li><a href="#">JavaScript</a></li>
-                                <li><a href="#">jQuery</a></li>
-                                <li><a href="#">HTML5</a></li>
-                                <li><a href="#">CSS3</a></li>
-                                <li><a href="#">SCSS</a></li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3>Backend </h3>
-                            <ul>
-                                <li><a href="#">Node.js</a></li>
-                                <li><a href="#">MongoDB</a></li>
-                                <li><a href="#">Mongoose</a></li>
-                                <li><a href="#">Express</a></li>
-                                <li><a href="#">NoSQL</a></li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3>Prototyping</h3>
-                            <ul>
-                                <li><a href="#">Figma</a></li>
-                                <li><a href="#">Adobe XD</a></li>
-                                <li><a href="#">Zeplin</a></li>
-                                <li><a href="#">Adobe Photoshop</a></li>
-                                <li><a href="#">Adobe Illustrator</a></li>
-                            </ul>
-                        </div>
-                        <div>
-                            <h3>Communication</h3>
-                            <ul>
-                                <li><a href="#">Git</a></li>
-                                <li><a href="#">GitHub</a></li>
-                                <li><a href="#">Slack</a></li>
-                                <li><a href="#">JIRA</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <address className="footer-right">
-                        2024 주현정 Portfolio. React Typescript.<br />All Right Reserved
-                    </address>
+            <div className="footer-menu">
+                <div>
+                    <h3>Frontend</h3>
+                    <ul>
+                        <li>React.js</li>
+                        <li>React Query</li>
+                        <li>Redux</li>
+                        <li>TypeScript</li>
+                        <li>JavaScript</li>
+                        <li>HTML5</li>
+                        <li>SCSS</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Prototyping</h3>
+                    <ul>
+                        <li>Figma</li>
+                        <li>Adobe XD</li>
+                        <li>Zeplin</li>
+                        <li>Adobe Photoshop</li>
+                        <li>Adobe Illustrator</li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Project Management</h3>
+                    <ul>
+                        <li>Git</li>
+                        <li>GitHub</li>
+                        <li>Slack</li>
+                        <li>JIRA</li>
+                    </ul>
                 </div>
 
+            </div>
+            <address className="footer-right">
+                주현정 MovieApp Portfolio. React TypeScript.<br />2024 All Right Reserved
+            </address>
         </footer>
     );
 };
